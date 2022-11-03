1abe173eaf894c43f8a1e10a5097c7d899b61ee0
Jacob Conacher <jake.conacher@gmail.com>
2022-11-03 15:27:37 -0500

    Fully coded the two needed view models

---
+++ PresidentsList/Models/USPresident.swift
@@ -0,0 +1,10 @@
+//
+//  USPresident.swift
+//  PresidentsList
+//
+//  Created by Jacob Conacher on 11/3/22.
+//
+
+import Foundation
+
+
--- PresidentsList/View Models/PresidentsViewModel.swift
+++ PresidentsList/View Models/PresidentsViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+struct PresidentsViewModel {
+    var president: USPresident
+    
+    var name: String {
+        return president.name
+    }
+    
+    var number: Int {
+        return president.number
+    }
+    
+    var startDate: String {
+        return president.startDate
+    }
+    
+    var endDate: String {
+        return president.endDate
+    }
+    
+    var nickname: String {
+        return president.nickname
+    }
+    
+    var party: String {
+        return president.party
+    }
+    
+    static var `default`: PresidentsViewModel {
+        let president = USPresident(name: "Andrew Jackson", number: 7, startDate: "March 4, 1829", endDate: "March 3, 1837", nickname: "\"Old Hickory\"", party: "Democrat")
+        return PresidentsViewModel(president: president)
+    }
+}
