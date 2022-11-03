82cf298af4e92fef49f653e1205f9effa97987b7
Jacob Conacher <jake.conacher@gmail.com>
2022-11-03 15:31:52 -0500

    Fully coded USPresident

--- PresidentsList/Models/USPresident.swift
+++ PresidentsList/Models/USPresident.swift
@@ -7,4 +7,20 @@
 
 import Foundation
 
-
+struct USPresident: Decodable {
+    var name = ""
+    var number = ""
+    var startDate = ""
+    var endDate = ""
+    var nickname = ""
+    var party = ""
+    
+    private enum CodingKeys: String, CodingKey {
+        case name = "Name"
+        case number = "Number"
+        case startDate = "Start Date"
+        case endDate = "End Date"
+        case nickname = "Nickname"
+        case party = "Political Party"
+    }
+}
